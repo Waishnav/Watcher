4c53701e1505273ff96ac4158aaf5df339c20b16
Waishnav <waishnavdeore@gmail.com>
2022-03-18 10:22:28 +0530

    issue resolved : watch_log stoped working after 24:00

--- src/Watcher/watch_log.py
+++ src/Watcher/watch_log.py
@@ -35,6 +35,13 @@ def append_line_in_csv(date, closed_time, window_name):
 afk = False
 def log_creation():
     global afk
+
+    filename = "/home/"+os.getlogin()+"/.cache/Watcher/raw_data/"+get_date()+".csv"
+    if not(os.path.isfile(filename)):
+        with open(filename, 'a') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter='\t')
+            csvwriter.writerow([get_time(), "00:00:00", ""])
+
     append_line_in_csv(get_date(), get_time(), "User-logged-in")
     while True:
         filename = "/home/"+os.getlogin()+"/.cache/Watcher/raw_data/"+get_date()+".csv"
